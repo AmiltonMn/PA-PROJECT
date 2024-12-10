5be2de8d5576c94ae297420f8d604fd24b3afb2a
AmiltonMn <amilton.moreiran@hotmail.com>
2024-12-10 14:23:08 -0300

    Ta limpo o projeto, já pode codar!

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,12 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { Platform } from 'react-native';
+
+export default function TabLayout() {
+
+  return (
+    <>
+    
+    </>
+  );
+}
