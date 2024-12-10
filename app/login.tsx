4247365ffafcd93abaa735fa6f3ca18a1966ee66
Fernandohka <fernando2005andrade@gmail.com>
2024-12-10 17:06:11 -0300

    login

--- app/login.tsx
+++ app/login.tsx
@@ -1,24 +1,119 @@
-import { StyleSheet, Image, Platform, View, Text } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import { useState } from 'react';
+import { Dimensions, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
-export default function TabTwoScreen() {
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [emailUsuario, setEmailUsuario] = useState<string>("");
+  const [pass, setPass] = useState<string>("");
+  
   return (
-    <View>
-        <Text>
-            Batatinha
-        </Text>
+    <View style={styles.tela}>
+      <View style={styles.quadrado}>
+        <View>Imagem bla bla bla</View>
+        <Text style={styles.login}>Login</Text>
+        <View>
+          <Text style={styles.titulo}>Usuário ou e-mail</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={setEmailUsuario}
+            value={emailUsuario}
+            placeholder="Digite seu e-mail ou usuário"
+            placeholderTextColor={'#A8A6A6'}
+            keyboardType="default"
+          />
+        </View>
+        <View>
+          <Text style={styles.titulo}>Senha</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={setPass}
+            value={pass}
+            placeholder="Digite sua senha"
+            placeholderTextColor={'#A8A6A6'}
+            keyboardType="default"
+            secureTextEntry={true}
+          />
+        </View>
+
+        <TouchableOpacity style={styles.botao}>
+          <Text style={styles.textBotao}>Logar</Text>
+        </TouchableOpacity>
+        <View style={styles.textos}>
+          <Text style={styles.textConta}>Não tem conta? </Text>
+          <Text style={styles.clique}>Clique aqui</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
+const {width, height} = Dimensions.get('window')
+
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
+  tela: {
+    width: width,
+    height: height,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#192870'
   },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
+  titulo: {
+    fontWeight: 'bold',
+    fontSize: 13,
   },
-});
+  login: {
+    fontWeight: 'bold',
+    fontSize: 17,
+    marginVertical: 12
+  },
+  input: {
+    backgroundColor: '#EBEBEB',
+    paddingHorizontal: 8,
+    paddingVertical: 5,
+    height: 32,
+    width: 228,
+    borderRadius: 5,
+    borderColor: '#F29F05',
+    borderWidth: 1,
+    marginTop: 4,
+    marginBottom: 12,
+    fontSize: 13
+  },
+  botao: {
+    width: 99,
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F29F05',
+    borderRadius: 5,
+    marginTop: 10,
+    marginBottom: 22
+  },
+  textBotao: {
+    color: '#FFFFFF',
+    fontSize: 17,
+  },
+  textConta: {
+    fontSize: 11
+  },
+  clique: {
+    fontSize: 11,
+    color: '#192870',
+    fontWeight: 'bold'
+  },
+  textos: {
+    flexDirection: 'row'
+  },
+  quadrado: {
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 44,
+    paddingBottom: 41,
+    borderRadius: 5
+  }
+})
