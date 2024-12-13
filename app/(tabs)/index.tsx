a9a8d81495ca5936a27ef37842edf56680ce858b
Fernandohka <fernando2005andrade@gmail.com>
2024-12-13 14:35:11 -0300

    Sobre nós

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,22 @@
-import { Image, StyleSheet, Platform, View, Dimensions } from 'react-native';
+import { Image, StyleSheet, Platform, View, Dimensions, Text } from 'react-native';
 
 export default function HomeScreen() {
   return (
     <View style={styles.tela}>
+      <View style={styles.quadrados}>
+        <View style={styles.quadrado}>
+          <Text style={styles.title}>Nossa História</Text>
+          <Text style={styles.desc}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut leo leo. Nulla lobortis dolor at vehicula iaculis. Ut eget lacus in dolor varius vestibulum nec at odio. Quisque bibendum ornare sem, et ornare magna tempor eget. </Text>
+        </View>
+        <View style={styles.quadrado}>
+          <Text style={styles.title}>Visão</Text>
+          <Text style={styles.desc}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut leo leo. Nulla lobortis dolor at vehicula iaculis. Ut eget lacus in dolor varius vestibulum nec at odio. Quisque bibendum ornare sem, et ornare magna tempor eget. </Text>
+        </View>
+        <View style={styles.quadrado}>
+          <Text style={styles.title}>Missão</Text>
+          <Text style={styles.desc}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut leo leo. Nulla lobortis dolor at vehicula iaculis. Ut eget lacus in dolor varius vestibulum nec at odio. Quisque bibendum ornare sem, et ornare magna tempor eget. </Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -29,6 +43,23 @@ const styles = StyleSheet.create({
   tela: {
     width: width,
     height: height,
-    backgroundColor: '#192870'
+    backgroundColor: '#192870',
   },
+  title: {
+    color: "#F29F05",
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  desc: {
+    fontSize: 20
+  },
+  quadrado: {
+    backgroundColor: "#FFFFFF",
+    padding: 16,
+    borderRadius: 5
+  },
+  quadrados: {
+    padding: 16,
+    gap: 17
+  }
 });
